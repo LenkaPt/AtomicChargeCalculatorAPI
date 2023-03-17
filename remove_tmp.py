abcc39dcb80174b634d35584cc3af5e9a30507d5
LenkaPt <90931862+LenkaPt@users.noreply.github.com>
2023-03-17 09:40:58 +0100

    Delete remove_tmp.py
    removing of tmp done by python function, not by a service anymore

--- remove_tmp.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-import subprocess
-
-
-def main():
-    tmp = Path('/home/tmp')
-    for folder in tmp.iterdir():
-        subprocess.run(['sudo', 'rm', '-r', str(folder)])
-main()
