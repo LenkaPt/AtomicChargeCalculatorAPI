8701140aa5bd9f372928b3ff3f1e55cca4e9c798
LenkaPt <ptacnikova.lenka@email.cz>
2023-04-01 11:39:33 +0200

    user and paths changed from chargefw2 to api_acc2

--- utils/api_acc2.wsgi
+++ utils/api_acc2.wsgi
@@ -1,5 +1,5 @@
 import sys
 
-sys.path.insert(0, '/home/chargefw2/api_acc2/src')
+sys.path.insert(0, '/home/api_acc2/api_acc2/src')
 
 from api_acc2 import app as application
--- utils/config_file.sh
+++ utils/config_file.sh
@@ -2,50 +2,22 @@
 sudo apt-get update
 sudo apt-get -y upgrade
 
-# create new user chargefw2
-sudo useradd chargefw2
-sudo mkdir /home/chargefw2
+# create new user api_acc2
+sudo useradd api_acc2
+sudo mkdir /home/api_acc2
 
-# install apache2
-sudo apt-get install -y apache2
-# sudo reboot
+# install apache2, mod_wsgi
+sudo apt-get install -y apache2 libapache2-mod-wsgi-py3
 
-# install mod_wsgi
-sudo apt-get install -y libapache2-mod-wsgi-py3
+# install flask, pip
+sudo apt install -y python3-flask python3-pip
 
-# install flask
-sudo apt install -y python3-flask
-
-# install pip
-sudo apt install -y python3-pip
 
 # chargefw2 deployment
 # boost
 sudo apt-get install -y libboost-filesystem-dev libboost-system-dev libboost-program-options-dev
-# cmake
-sudo apt-get install -y cmake
-# g++
-sudo apt-get install -y g++
-# eigen
-sudo apt-get install -y libeigen3-dev
-# fmt
-sudo apt-get install -y libfmt-dev
-# JSON for Modern C++: 
-sudo apt-get install -y nlohmann-json3-dev
-# pybind11
-sudo apt-get install -y python3-pybind11
-# clang
-sudo apt-get install -y clang
-# gemmi
-# git clone --depth 1 https://github.com/project-gemmi/gemmi.git
-# cp -r gemmi/include/gemmi /usr/include
-sudo apt-get install -y gemmi-dev
-sudo apt install -y gemmi
-# nanoflann
-# git clone --depth 1 --branch v1.3.2 https://github.com/jlblancoc/nanoflann.git /usr/include
-sudo apt-get install -y libnanoflann-dev
-# pegl
-sudo apt-get install -y tao-pegtl-dev
+sudo apt-get install -y cmake g++ libeigen3-dev libfmt-dev nlohmann-json3-dev python3-pybind11
+sudo apt-get install -y clang gemmi-dev gemmi libnanoflann-dev tao-pegtl-dev
 
 # chargefw2
 sudo apt install -y git
@@ -69,21 +41,18 @@ sudo pip install flask-restx
 # openbabel installation
 sudo apt-get install -y openbabel
 
-# flask Limiter
-# sudo pip install Flask-Limiter
-
 # dos2unix installation
 sudo apt-get install -y dos2unix
 
 # API
-sudo mkdir /home/chargefw2/api_acc2
-sudo git clone https://github.com/LenkaPt/AtomicChargeCalculatorAPI.git /home/chargefw2/api_acc2
+sudo mkdir /home/api_acc2/api_acc2/logs
+sudo git clone https://github.com/LenkaPt/AtomicChargeCalculatorAPI.git /home/api_acc2/api_acc2
 sudo mkdir /home/tmp
-sudo chown -R chargefw2:chargefw2 /home/chargefw2
-sudo chown -R chargefw2:chargefw2 /home/tmp
+sudo chown -R api_acc2:api_acc2 /home/api_acc2
+sudo chown -R api_acc2:api_acc2 /home/tmp
 
 # enable api configuration
-sudo mv /home/chargefw2/api_acc2/utils/api_acc2.conf /etc/apache2/sites-available/
+sudo mv /home/api_acc2/api_acc2/utils/api_acc2.conf /etc/apache2/sites-available/
 cd /etc/apache2/sites-available
 sudo a2ensite api_acc2.conf
 
